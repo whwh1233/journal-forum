b0de3ab5bc37c98ce22d154d2b3b5fdef7a49f7f
whwh1233 <1249156074@qq.com>
2026-03-14 00:55:58 +0800

    chore: add zod, husky and configure shared path alias

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,8 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
+        '@': path.resolve(__dirname, './src'),
+        '@shared': path.resolve(__dirname, './shared')
       }
     }
   };
